bc12fd295f4818b4b0eadc023973ff73335eb39b
Robin Radic <robin@radic.nl>
2017-11-08 03:29:05 +0100

    xml update

---
+++ README.md
@@ -0,0 +1,13 @@
+# Generators
+
+
+## Development
+
+#### node-gyp error
+- [libxmljs github issue](https://github.com/nodejs/node-gyp/issues/454)
+- [node-gyp github issue](https://github.com/nodejs/node-gyp/issues/454)
+
+After installing run
+```sh
+sudo npm i libxmljs --dev --unsafe-perm
+```
--- gulpfile.ts
+++ gulpfile.ts
@@ -5,46 +5,71 @@ import * as ts from 'typescript';
 // noinspection ES6UnusedImports
 import * as shelljs from 'shelljs';
 import * as globule from 'globule';
-import { basename, resolve } from 'path';
+import { basename, join, resolve } from 'path';
 import * as _ from 'lodash';
 import * as pump from 'pump';
+import { statSync } from 'fs';
+import * as through2 from 'through2';
 
 const clean = require('gulp-clean')
 
 type TSProjectOptions = Settings & ts.CompilerOptions
 
-const c            = {
-    pkg                    : require('./package.json'),
-    lerna                  : require('./lerna.json'),
-    tsconfig               : require('./tsconfig.json'),
-    defaultTsProjectOptions: <TSProjectOptions>{
-        typescript     : ts,
-        declaration    : true,
-
-        inlineSourceMap: false,
-        inlineSources  : false
+const c = {
+    pkg  : require('./package.json'),
+    lerna: require('./lerna.json'),
+    ts   : {
+        config    : require('./tsconfig.json'),
+        taskPrefix: 'ts',
+        defaults  : <TSProjectOptions>{
+            typescript     : ts,
+            declaration    : true,
+            inlineSourceMap: false,
+            inlineSources  : false
+        }
     }
 };
-const packagePaths = globule.find('packages/*').map(path => resolve(path));
+
+
+const packagePaths = globule
+    .find('packages/*')
+    .map(path => resolve(path))
+    .filter(path => statSync(path).isDirectory());
 const packages     = packagePaths.map(path => ({
     path: {
         toString: () => resolve(path),
-        to      : (...args) => resolve.apply(resolve, [ path ].concat(args)),
+        to      : (...args: string[]) => resolve(path, join.apply(join, args)),
         glob    : (pattern: string | string[]) => _.toArray(pattern).map(pattern => resolve(path, pattern))
     },
     name: basename(path)
 }));
-
+through2()
 const createTsTask = (name, pkg, dest, tsProject: TSProjectOptions = {}) => {
-    gulp.task('clean:' + name, () => pump(gulp.src(pkg.path.to(dest)), clean()))
+    gulp.task('clean:' + name, () => {
+        let paths = []
+            .concat(
+                globule
+                    .find(join(pkg.path.to('src'), '**/*'))
+                    .filter(path => statSync(path).isDirectory())
+                    .map(path => path.replace(pkg.path.to('src'), pkg.path))
+            )
+            .concat(globule.find(join(pkg.path.to('*.js'))))
+        let proj = gts.createProject(pkg.path.to('tsconfig.json'));
+        paths.push(proj.options.declarationDir)
+        gulp.src(paths).pipe(clean())
+        // console.log(paths);
+        // gulp.src().pipe(clean())
+        // console.log({name, dest, pkg, path:})
+        // gulp.src()).pipe(clean())
+    });
     gulp.task('build:' + name, () => pump(
-        gulp.src(pkg.path.glob('src/**/*.ts')),
-        gts.createProject(pkg.path.to('tsconfig.json'), _.merge(c.defaultTsProjectOptions, tsProject))(),
+        gulp.src(pkg.path.glob('src/**/*')),
+        gts.createProject(pkg.path.to('tsconfig.json'), _.merge(c.ts.defaults, tsProject))(),
         gulp.dest(pkg.path.to(dest))
     ))
 }
 let packageNames   = packages.map(pkg => {
-    createTsTask(`ts:${pkg.name}`, pkg, '/', { target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS });
+    createTsTask(`${c.ts.taskPrefix}:${pkg.name}`, pkg, '/', { target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS });
     // createTsTask(`ts:${pkg.name}:es`, pkg, 'es', { declaration: true, declarationDir: 'types', target: ts.ScriptTarget.ES5, module: ts.ModuleKind.ES2015 });
     // [ 'build', 'clean' ].forEach(prefix => gulp.task(`${prefix}:ts:${pkg.name}`, [
     //     `${prefix}:ts:${pkg.name}:lib`,
@@ -52,9 +77,11 @@ let packageNames   = packages.map(pkg => {
     // ]));
     return pkg.name
 });
-[ 'build', 'clean' ].map(prefix => gulp.task(`${prefix}:ts`, packageNames.map(name => `${prefix}:ts:${name}`)))
+[ 'build', 'clean' ].map(prefix => gulp.task(`${prefix}:ts`, packageNames.map(name => `${prefix}:${c.ts.taskPrefix}:${name}`)))
 
-gulp.task('build', ['build:ts'])
-gulp.task('clean', ['clean:ts'])
 
-console.dir({ c, packagePaths, packages })
+gulp.start('clean:ts')
+// gulp.src([
+//     '.idea/libraries/tsconfig_roots.xml',
+//     '.idea/jsLibraryMappings.xml'
+// ]).pipe(clean())
